a19b6044935c9248ff259d08afcbd615809e51e6
Parth Patel <Parth.Patel@dal.ca>
2019-02-06 14:36:56 -0400

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,10 +1,12 @@
+"""function to create a list of "access-list" for "global_access" and "fw-management_access_in"""
+
 def access_lst(file_name):
 	fout = open(file_name)
 	lst=[]
 	for item in fout:
 		item=item.strip()
 		for i in item.split():
-			if i=='global_access' or i=='fw-management_access_in':	
+			if i=='global_access' or i=='fw-management_access_in':	#if the line contains global-access or fw-management it will add that line to list
 				lst.append(item)
 	return lst
 
